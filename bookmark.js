527b0798c3bde604f0f20bdf9bd407862f846e4a
j-a-13 <52170956+j-a-13@users.noreply.github.com>
2020-12-18 10:37:14 +0000

    Update bookmark.js

--- bookmark.js
+++ bookmark.js
@@ -1,3 +1,3 @@
 var script = document.createElement("script");
-script.src = "https://raw.githubusercontent.com/j-a-13/coolMathGamesHacks/main/fullscreen.js";
+script.src = "https://cdn.jsdelivr.net/gh/j-a-13/coolMathGamesHacks/main/bookmark.js";
 document.body.append(script);
