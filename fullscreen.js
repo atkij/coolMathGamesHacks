9fcb8c0a36bf3d1ed139d148a192a0d6d2af562e
j-a-13 <52170956+j-a-13@users.noreply.github.com>
2021-01-11 12:31:20 +0000

    Update fullscreen.js

--- fullscreen.js
+++ fullscreen.js
@@ -15,7 +15,7 @@ fullscreen.id = "fullscreen-hack";
 fullscreen.href = "/"
 fullscreen.addEventListener("click", function (e) {
   e.preventDefault()
-  let game = document.getElementById("html5game")
+  let game = document.getElementById("swfgamewrapper").firstChild();
   game.requestFullscreen();
 });
 //add fullscreen link to appropriate place
